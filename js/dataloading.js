a5f832951432d79df02858b03361b9f50f8d5871
Lei Yang <lei@seas.upenn.edu>
2014-11-23 18:07:41 -0500

    data update

--- js/dataloading.js
+++ js/dataloading.js
@@ -5,11 +5,15 @@ function loadWorldPins( callback ){
 	// Where do we get the data?
 	xhr.open( 'GET', latlonFile, true );
 
+	//ra
+	xhr.overrideMimeType("application/json");
+
 	// What do we do when we have it?
 	xhr.onreadystatechange = function() {
 	  // If we've received the data
-	  if ( xhr.readyState === 4 && xhr.status === 200 ) {
+	  //if ( xhr.readyState === 4 && xhr.status === 200 ) {
 	      // Parse the JSON
+	    if ( xhr.readyState === 4){
 	      latlonData = JSON.parse( xhr.responseText );
 	      if( callback )
 	      	callback();				     
@@ -21,14 +25,19 @@ function loadWorldPins( callback ){
 }
 
 function loadContentData(callback){	
-	var filePath = "categories/All.json";
+	var filePath = "categories/All-test.json";
 	filePath = encodeURI( filePath );
 	// console.log(filePath);
 			
 	xhr = new XMLHttpRequest();
 	xhr.open( 'GET', filePath, true );
+
+	//ra
+	xhr.overrideMimeType("application/json");
+
 	xhr.onreadystatechange = function() {
-		if ( xhr.readyState === 4 && xhr.status === 200 ) {
+		if(xhr.readyState === 4){
+		//if ( xhr.readyState === 4 && xhr.status === 200 ) {
 	    	timeBins = JSON.parse( xhr.responseText ).timeBins;
 		
 			maxValue = 0;
@@ -49,8 +58,13 @@ function loadContentData(callback){
 function loadCountryCodes( callback ){
 	cxhr = new XMLHttpRequest();
 	cxhr.open( 'GET', isoFile, true );
+
+	//ra
+	cxhr.overrideMimeType("application/json");
+
 	cxhr.onreadystatechange = function() {
-		if ( cxhr.readyState === 4 && cxhr.status === 200 ) {
+	if(cxhr.readyState === 4){
+	//	if ( cxhr.readyState === 4 && cxhr.status === 200 ) {
 	    	countryLookup = JSON.parse( cxhr.responseText );	
 	    	console.log("loaded country codes");
 	    	callback();
